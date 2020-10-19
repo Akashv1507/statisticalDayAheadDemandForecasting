484c63765cd90de6e80811b40ea31edc74164ba5
Akash Verma <akashv1507@gmai.com>
2020-10-19 13:12:50 +0530

    single batch file addedd

--- run.bat
+++ run.bat
@@ -2,3 +2,6 @@ call project_env\Scripts\activate.bat
 call python index_insMinwiseDemand.py
 call python index_insBlockwiseDemand.py
 call python index_insStagingBlockwiseDemand.py --want_updation True
+REM forecast adjustment
+call python index_adjustmentBeforeForecast.py
+call python index_dayAheadForecast.py
